121ab914a12c1a89c27989353e3c7f1a75cd722e
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-03-12 13:55:07 -0400

    update

---
+++ old/flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "a bash script that takes an HTML file and uses Chromium to
+  render it as a PDF. Chromium is not from nix right now because of Darwin";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-stable.url = "nixpkgs/release-24.11";
+    utils.url = "github:numtide/flake-utils";
+
+    chromium-html-to-pdf = {
+      url = "github:friedenberg/chromium-html-to-pdf";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-stable, utils, chromium-html-to-pdf }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        name = "pa6e-markdown-to-html";
+        buildInputs = with pkgs; [
+          imagemagick
+          pandoc
+          chromium-html-to-pdf.packages.${system}.html-to-pdf
+        ];
+        pa6e-markdown-to-html = (
+          pkgs.writeScriptBin name (builtins.readFile ./markdown-to-html.bash)
+        ).overrideAttrs(old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        });
+
+        # to include all the templates and styles
+        src = ./.;
+
+      in rec {
+        packages.pa6e-markdown-to-html = pkgs.symlinkJoin {
+          name = name;
+          paths = [ pa6e-markdown-to-html src ] ++ buildInputs;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+        };
+
+        defaultPackage = packages.pa6e-markdown-to-html;
+
+        devShells.default = pkgs.mkShell {
+          packages = (with pkgs; [
+            uv
+            imagemagick
+            pandoc
+            chromium-html-to-pdf.packages.${system}.html-to-pdf
+          ]);
+
+          inputsFrom = [];
+        };
+      }
+    );
+}
